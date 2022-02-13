6dab29e213005dc24753ee6cdc4d5bd770f94dc2
dbogt <59750436+dbogt@users.noreply.github.com>
2022-02-13 02:50:30 -0500

    initial form

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,50 @@ from deta import Deta
 
 st.title("Oscars 2022 Predictions")
 
+deta = Deta(st.secrets["project_key"])
+db = deta.Base("oscar_bets_test")
+
+best_movies = ['Belfast','CODA',"Don't Look Up","Drive My Car","Dune","King Richard","Licorice Pizza","Nightmare Alley","The Power of the Dog","West Side Story"]
+best_directors = ['Kenneth Branagh – Belfast',
+                    'Ryusuke Hamaguchi – Drive My Car',
+                    'Paul Thomas Anderson – Licorice Pizza',
+                    'Jane Campion – The Power of the Dog',
+                    'Steven Spielberg – West Side Story']
+best_actors = ['Javier Bardem – Being the Ricardos as Desi Arnaz',
+                'Benedict Cumberbatch – The Power of the Dog as Phil Burbank',
+                'Andrew Garfield – Tick, Tick... Boom! as Jonathan Larson',
+                'Will Smith – King Richard as Richard Williams',
+                'Denzel Washington – The Tragedy of Macbeth as Lord Macbeth']
+best_actress = ['Jessica Chastain – The Eyes of Tammy Faye as Tammy Faye Bakker',
+                'Olivia Colman – The Lost Daughter as Leda Caruso',
+                'Penélope Cruz – Parallel Mothers as Janis Martínez Moreno',
+                'Nicole Kidman – Being the Ricardos as Lucille Ball',
+                'Kristen Stewart – Spencer as Diana, Princess of Wales']
+
+st.write("Enter your predictions below")
+with st.form("my_picks"):
+    st.write("Predictions Forms")
+    user_name = st.text_input("Your name (First Last)")
+    user_email = st.text_input("Your email")
+    password = st.text_input("A 'safe word' to retrieve your picks (do NOT use a real password)")
+    best_movie_pick = st.selectbox('What movie will win the best picture?',best_movies)
+    best_director_pick = st.selectbox('Who will win best director?', best_directors)
+    best_actor_pick = st.selectobx('Who will win the best actor?', best_actors)
+    best_actress_pick = st.selectobx('Who will win the best actress?', best_actress)
+    submit_btn = st.form_submit_button("Save Picks")
+
+if submitted:
+    st.write("Thank you!")
+    st.balloons()
+    db.put({"name": user_name,"email": user_email,
+            'password':password,
+            'best_movie':best_movie_pick,
+            'best_director':best_director_pick,
+            'best_actor':best_actor_pick,
+            'best_actress':best_actress_pick})
+
+db_content = db.fetch().
+df = pd.DataFrame(db_content)
+if st.checkbox('Show predictions by person'):
+    st.dataframe(df)
+
