8273a854d446ae09bcb786d74688d26ae8de8e54
dbogt <59750436+dbogt@users.noreply.github.com>
2022-02-15 01:47:08 -0500

    bug

--- streamlit_app.py
+++ streamlit_app.py
@@ -189,7 +189,7 @@ else:
     if quizMode == "Easy":
         with st.form("easy_quiz"):
             for x in range(1,11):
-                answerPicks[str(x)] = st.selectbox(answersOptions)
+                answerPicks[str(x)] = st.selectbox(answerOptions)
             submit_quiz = st.form_submit_button("Submit Answers")
             if submit_quiz:
                 st.write(answerPicks)
