8b49418240c60d663814258cc2a0de6173e71977
Bogdan Tudose <59750436+dbogt@users.noreply.github.com>
2024-01-28 10:41:36 -0500

    Update 2_Oscars 2023.py
    removed predictions

--- pages/2_Oscars 2023.py
+++ pages/2_Oscars 2023.py
@@ -1,20 +1,11 @@
 #%% Import Packages
 import streamlit as st
 import pandas as pd 
-from deta import Deta
 import plotly.express as px
 
 #%% Streamlit Config Settings
 st.set_page_config(layout="wide",page_title='Oscars Predictions')
 
-#%% Deta Database Connection
-deta = Deta(st.secrets["project_key"])
-db = deta.Base("oscar_bets_test")
-
-cols = ['name','email','password','city','best_movie','best_director','best_actor','best_actress']
-safeCols = cols.copy()
-safeCols.remove('email')
-safeCols.remove('password')
 
 #%% Functions
 @st.cache
@@ -55,16 +46,9 @@ def oscars_vs_bafta():
     return nominations
 
     
-def grab_predictions():
-    db_content = db.fetch().items
-    df = pd.DataFrame(db_content)
-    df = df[cols]
-    return df
 
 #%% Import Data
 nominees = grab_nominees2023() 
-df = grab_predictions()
-emails = df['email'].to_list()
 
 #Nominees options for dropdowns
 best_movies = nominees[nominees['Category']=='Best Picture']['Nominee'].drop_duplicates().sort_values()
@@ -112,65 +96,4 @@ if selectPage == "Oscar 2023 Nominees":
                      hover_name='Film', title='2023 Oscar Nominations vs BAFTA Nominations')
     st.plotly_chart(figOscVsBAFTA) #strip plot creates a jitter plot (slightly offsets markers for overlaping pts)
     st.write(nominations)
-
-elif selectPage == "Oscar 2023 Predictions":
-    st.title("Oscars 2023 Predictions")
-    if st.checkbox('Make Predictions'):
-        st.header("Predictions")
-        st.write("Enter your predictions below")
-        with st.form("my_picks"):
-            st.write("Predictions Forms")
-            user_name = st.text_input("Your name (First Last)")
-            user_email = st.text_input("Your email")
-            user_city = st.text_input("Your city")
-            password = st.text_input("A 'safe word' to retrieve your picks (do NOT use a real password)", type="password")
-            best_movie_pick = st.selectbox('What movie will win the best picture?',best_movies)
-            best_director_pick = st.selectbox('Who will win best director?', best_directors)
-            best_actor_pick = st.selectbox('Who will win the best actor?', best_actors)
-            best_actress_pick = st.selectbox('Who will win the best actress?', best_actress)
-            submit_btn = st.form_submit_button("Save Picks")
-
-            if submit_btn:
-                checkOK = True
-                if user_email in emails:
-                    currentUserDF = df[df['email'] == user_email] 
-                    checkPass = currentUserDF.iloc[0]['password']
-                    if checkPass != password:
-                        checkOK= False
-
-                if checkOK:
-                    st.write("Thank you!")
-                    st.balloons()
-                    db.put({"name": user_name, "email": user_email, 'password':password,
-                            'best_movie':best_movie_pick, 'best_director':best_director_pick,
-                            'best_actor':best_actor_pick, 'best_actress':best_actress_pick,
-                            'city':user_city, 'key':user_email})
-                    df = grab_predictions()
-                else:
-                    st.write("Incorrect password, try again or submit with another email!")
-
-    if st.checkbox('Show predictions by person'):
-        safeDF = df[safeCols]
-        adminPass = st.text_input('Admin Password',type="password")
-        if adminPass == st.secrets['admin_pass']:
-            st.dataframe(df)
-        else:
-            st.dataframe(safeDF)
-
-    if st.checkbox('Show summary picks'):
-        df['Count'] = 1
-        summaryDict = {'Best Movie':'best_movie',
-                        'Best Director':'best_director',
-                        'Best Actor':'best_actor',
-                        'Best Actress':'best_actress'}
-        summaryPicks = st.multiselect("Pick categories to show",summaryDict.keys())
-        outputType = st.radio('Output type',('Tables','Bar Charts'))
-        for pick in summaryPicks:
-            colName = summaryDict[pick]
-            st.header(pick)
-            if outputType == 'Tables':
-                st.write(df[colName].value_counts())
-            else:
-                fig = px.bar(df, x=colName, y='Count', color="city", hover_name="name", barmode='stack', labels={colName:pick}).update_xaxes(categoryorder="total descending")
-                st.plotly_chart(fig)
              
