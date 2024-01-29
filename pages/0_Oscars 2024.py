797788f133427c08c8802052627a472d5aebdec3
Bogdan Tudose <59750436+dbogt@users.noreply.github.com>
2024-01-29 00:11:36 -0500

    Update 0_Oscars 2024.py

--- pages/0_Oscars 2024.py
+++ pages/0_Oscars 2024.py
@@ -25,7 +25,7 @@ with st.expander("See app info"):
 
 
 #%% Pick Year
-year = st.sidebar.selectbox("Select Year", ('2022','2023','2024'))
+year = st.sidebar.selectbox("Select Year", ('2024','2023','2022'))
 csvs = {'2022': "Oscars2022_Nominees.csv", '2023':"Oscars2023_Nominees.csv", '2024':"Oscars2024_Nominees.csv" }
 baftas = {'2022':'75th', '2023':'76th', '2024':'77th'}
 oscars = {'2022':'94th', '2023':'95th', '2024':'96th'} 
